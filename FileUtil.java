1184e9c350aab572889b48aca1cb1cba78fd8fce
AnotherBleach <2325539291@qq.com>
2018-01-18 11:06:18 +0800

    FileUtil

---
+++ FileUtil.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+public class FileUtil 
+{
+        
+       public static long getFileSize(String file) throws IOException
+       {
+            return getFileSize(new File(file));
+
+       }
+        public static long getFileSize(File file) throws IOException
+    {
+        try(FileInputStream fis = new FileInputStream(file)){
+        long size = 0;
+        byte[] tmp = new byte[1024];
+        int b=0;
+        while((b = fis.read(tmp))!=-1)
+        {
+            
+            size+=b;
+
+        }
+        return size;
+    }
+    
+    }
+}
